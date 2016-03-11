6a09c340f66433c3f91e58c5153e9d0d4de6e18b
Mr.Ye <yelong@yelongdemacbook-pro.local>
2016-03-11 23:19:44 +0800

    Initial Commit

---
+++ UINavigationControll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationControll
+//
+//  Created by 叶龙 on 16/3/11.
+//  Copyright © 2016年 叶龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UINavigationControll/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UINavigationControll
+//
+//  Created by 叶龙 on 16/3/11.
+//  Copyright © 2016年 叶龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
